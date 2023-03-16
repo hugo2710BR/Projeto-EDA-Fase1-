689832534f7b27929d76e499eaaef54d6272dce2
hugoBR2710 <hugosoares2710@outlook.pt>
2023-03-16 20:16:03 +0000

    Estrturação do main

--- Gestor.c
+++ Gestor.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "Gestor.h"
 
@@ -13,4 +14,28 @@ Gestor* CriarGestor(int id, char* nome, char* email, char* senha) {
 
 Gestor* CriarGestorFixo(void) {
     return CriarGestor(1, "Maria Fernandes", "maria.fernandes@email.com", "senha123");
+}
+
+void AdicionarGestor(Gestor** gestores, int *numGestores) {
+    Gestor *novoGestor = (Gestor*) malloc(sizeof(Gestor));
+    
+    printf("Digite o ID do cliente: ");
+    scanf("%d", &novoGestor->id);
+    getchar();
+    
+    printf("Digite o nome do cliente: ");
+    fgets(novoGestor->nome, sizeof(novoGestor->nome), stdin);
+    strtok(novoGestor->nome, "\n");
+
+    printf("Digite a senha do gestor: ");
+    fgets(novoGestor->senha, sizeof(novoGestor->senha), stdin);
+    strtok(novoGestor->email, "\n");
+
+    system("cls");
+    printf("\nGestor com ID: %i foi adicionado com sucesso!\n", novoGestor->id);
+
+    (*numGestores)++;
+    *gestores = (Gestor*) realloc(*gestores, *numGestores * sizeof(Gestor));
+    (*gestores)[*numGestores - 1] = *novoGestor;
+    free(novoGestor);
 }
---
+++ Menu.h
@@ -0,0 +1,8 @@
+// menu.h
+#ifndef MENU_H
+#define MENU_H
+
+void menu();
+void GestaoClientes();
+
+#endif
